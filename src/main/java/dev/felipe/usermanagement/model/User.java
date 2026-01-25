849e5a2f16bdcb71b9c6655ac25b128b7b54429f
Felipe dos Anjos <dosanjosfelipe06@gmail.com>
2026-01-25 14:57:44 -0300

    building the User table

---
+++ src/main/java/dev/felipe/usermanagement/model/User.java
@@ -0,0 +1,26 @@
+package dev.felipe.usermanagement.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor()
+@Entity
+@Table(name = "users", uniqueConstraints = {@UniqueConstraint(columnNames  = "email")})
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 40)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+}
