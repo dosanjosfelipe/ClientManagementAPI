169f0375a6dcdd9c776d04a63f70aa1ef58758f5
Felipe dos Anjos <dosanjosfelipe06@gmail.com>
2026-01-25 14:53:55 -0300

    Configuring SecurityFilterChain

--- src/main/java/dev/felipe/usermanagement/config/WebConfig.java
+++ src/main/java/dev/felipe/usermanagement/config/WebConfig.java
@@ -1,11 +1,13 @@
 package dev.felipe.usermanagement.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import java.util.List;
 
+@Configuration
 public class WebConfig {
 
     @Bean
