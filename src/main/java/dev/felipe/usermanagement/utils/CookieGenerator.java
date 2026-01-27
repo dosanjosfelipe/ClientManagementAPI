de9d43e476c177e5b2fd4758b4ba1783e6cecf96
Felipe dos Anjos <dosanjosfelipe06@gmail.com>
2026-01-27 19:20:40 -0300

    refactor(Cookies) A utility class to create HttpOnly cookies

---
+++ src/main/java/dev/felipe/usermanagement/utils/CookieGenerator.java
@@ -0,0 +1,16 @@
+package dev.felipe.usermanagement.utils;
+
+import org.springframework.http.ResponseCookie;
+
+public class CookieGenerator {
+
+    public static ResponseCookie generateCookie(String name, String token, long maxAge) {
+        return ResponseCookie.from(name, token)
+                .httpOnly(true)
+                .secure(true)
+                .sameSite("Lax")
+                .path("/")
+                .maxAge(maxAge)
+                .build();
+    }
+}
