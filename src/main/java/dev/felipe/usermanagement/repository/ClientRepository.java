88ed1df0c33d8e2caabd677b153c021b84820481
Felipe dos Anjos <dosanjosfelipe06@gmail.com>
2026-01-29 08:25:30 -0300

    feat(Query) Adding a query for find a client by phone

--- src/main/java/dev/felipe/usermanagement/repository/ClientRepository.java
+++ src/main/java/dev/felipe/usermanagement/repository/ClientRepository.java
@@ -3,10 +3,16 @@ package dev.felipe.usermanagement.repository;
 import dev.felipe.usermanagement.model.Client;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface ClientRepository extends JpaRepository<Client, Long> {
     Optional<Client> findByEmail(@NotBlank @Email String email);
+
+    Optional<Client> findByPhone(@Pattern(regexp = "^[0-9]+$")
+                                 @NotBlank
+                                 @Size(min = 10, max = 11) String phone);
 }
